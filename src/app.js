3cc6dba70a1532a020d9c5bd0f46e12b9fc89f12
mishab <mishabsanu298@gmail.com>
2026-04-13 00:27:05 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(hpp());
 app.use(mongoSanitize());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://akod-delivery-note.vercel.app"],
+    origin: ["http://localhost:3000", "https://akod-erp.vercel.app"],
     credentials: true,
   })
 );
